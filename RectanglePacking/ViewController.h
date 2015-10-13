cbd5986f41d7e3fb189256442661321c5cf70f3d
Mariusz Graczkowski <mariusz.graczkowski@gmail.com>
2015-10-13 16:52:46 +0200

    Initial Commit

---
+++ RectanglePacking/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RectanglePacking
+//
+//  Created by Mariusz Graczkowski on 13.10.2015.
+//  Copyright © 2015 Mariusz Graczkowski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ RectanglePackingTests/RectanglePackingTests.m
@@ -0,0 +1,39 @@
+//
+//  RectanglePackingTests.m
+//  RectanglePackingTests
+//
+//  Created by Mariusz Graczkowski on 13.10.2015.
+//  Copyright © 2015 Mariusz Graczkowski. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface RectanglePackingTests : XCTestCase
+
+@end
+
+@implementation RectanglePackingTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
